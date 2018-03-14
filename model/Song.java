74f222130948d4ab488305bff0d7a3adbc16244f
Sureshdevendran <suresh@dhcp-10-142-185-108.uawifi.arizona.edu>
2018-03-14 11:00:05 -0700

    song collection and songs are completed

--- model/Song.java
+++ model/Song.java
@@ -1,5 +1,79 @@
 package model;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.Map;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 // Added to allow package model to exist on GitHub
 public class Song {
+  
+  private String title;
+  private Time songLength;
+  private String artist;
+  private File songFile;
+  
+  //Constructor one with 4 parameters
+  public Song(String tit, Long len, String art, String file) {
+	 this.title = tit;
+	 this.songLength.setTime(len);
+	 this.artist = art;
+	 this.songFile = new File("./songfiles"+file);
+  }
+  
+  //constructor two with 3 parameters
+  public Song(String tit, String art, String file) {
+	 this.title = tit;
+	 this.artist = art;
+	 this.songFile = new File("./songfiles"+file);
+	 
+	 try {
+		getDurationWithMp3Spi(songFile);
+	 } catch (UnsupportedAudioFileException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	 } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	 }
+  }
+  
+  public String getSongTitle() {
+	 return title;
+  }
+  
+  public Time getSongLength() {
+	 return songLength;
+  }
+  
+  public String getSongArtist() {
+	 return artist;
+  }
+  
+  public File getSongFile() {
+	 return songFile;
+  }
+  
+  //gets the time of the song
+  private void getDurationWithMp3Spi(File file) throws UnsupportedAudioFileException, IOException {
 
+    AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+    if (fileFormat instanceof AudioFileFormat) {
+        Map<?, ?> properties = ((AudioFileFormat) fileFormat).properties();
+        String key = "duration";
+        Long microseconds = (Long) properties.get(key);
+        int mili = (int) (microseconds / 1000);
+        int sec = (mili / 1000) % 60;
+        int min = (mili / 1000) / 60;
+        System.out.println("time = " + min + ":" + sec);
+        songLength.setTime(microseconds);
+    } else {
+        throw new UnsupportedAudioFileException();
+    }
+
+}
 }
--- model/SongCollection.java
+++ model/SongCollection.java
@@ -1,5 +1,40 @@
 package model;
 
-public class SongCollection {
+import java.util.LinkedList;
+import java.util.List;
 
+public class SongCollection {
+  
+  List<Song> songCollection;
+  
+  public SongCollection() {
+	 songCollection = new LinkedList<Song>();
+	 dummySongs();
+  }
+  
+  private void dummySongs() {
+	 Song a = new Song("Pokemon Capture", convertSecondsToMicro(5), "Pikachu", "Capture.mp3");
+	 Song b = new Song("Danse Macabre", convertSecondsToMicro(34), "Kevin MacLeod", "DanseMacabreViolinHook.mp3");
+	 Song c = new Song("Determined Tumbao", convertSecondsToMicro(20), "FreePlay Music", "DeterminedTumbao.mp3");
+	 Song d = new Song("Loping Sting", convertSecondsToMicro(5), "Kevin MacLeod", "LopingSting.mp3");
+	 Song e = new Song("Swing Cheese", convertSecondsToMicro(15), "FreePlay Music", "SwingCheese.mp3");
+	 Song f = new Song("The Curtain Rises", convertSecondsToMicro(28), "Kevin MacLeod", "TheCurtainRises.mp3");
+	 Song g = new Song("Untameable Fire", convertSecondsToMicro(4*60+42), "Pierre Langer", "UntameableFire.mp3");
+	 
+	 songCollection.add(a);
+	 songCollection.add(b);
+	 songCollection.add(c);
+	 songCollection.add(d);
+	 songCollection.add(e);
+	 songCollection.add(f);
+	 songCollection.add(g);
+  }
+  
+  public List<Song> getSongCollection() {
+	 return this.songCollection;
+  }
+   
+  private long convertSecondsToMicro(long sec) {
+	 return sec * 1000000;
+  }
 }
