d2701d5222e5e88e41f1d68f22155a9ce12b9639
rookmoose <35309755+rookmoose@users.noreply.github.com>
2018-03-14 10:06:02 -0700

    Player modified, Playerlist modified

--- model/Player.java
+++ model/Player.java
@@ -12,4 +12,10 @@ public class Player {
 	 this.numberOfSongChoices = choices;
   }
   
+  private void decrementSongChoices() {
+	  numberOfSongChoices--;
+  }
+  
+  
+  
 }
--- model/PlayerList.java
+++ model/PlayerList.java
@@ -1,5 +1,23 @@
 package model;
 
-public class PlayerList {
+import java.util.ArrayList;
 
+public class PlayerList {
+	
+	ArrayList<Player> listOfPlayers;
+	
+	/*
+	 * PlayerList() -- constructor for the class, instantiates ArrayList of Player objects
+	 * of size 5, hard codes Players.
+	 */
+	public PlayerList() {
+		listOfPlayers = new ArrayList<Player>(5);
+		listOfPlayers.add(new Player("Chris", "1", 3));
+		listOfPlayers.add(new Player("Devon", "22", 3));
+		listOfPlayers.add(new Player("River", "333", 3));
+		listOfPlayers.add(new Player("Ryan", "4444", 3));
+		listOfPlayers.add(new Player("Merlin", "7777777", 3));
+		
+	}// end constructor
+	
 }
