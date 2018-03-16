290051f60ca2e1a99b6ebc2cab3a97355a0c0609
rookmoose <35309755+rookmoose@users.noreply.github.com>
2018-03-16 09:40:31 -0700

    Partner 2 version

--- demoMediaPlayer/MyList.java
+++ demoMediaPlayer/MyList.java
@@ -1,4 +1,5 @@
 package demoMediaPlayer;
+
 /**
  * A simple list, partner 1 should implement this using a link structure.
  * Partner 2 should implement this using an array structure.
@@ -8,23 +9,46 @@ package demoMediaPlayer;
  * to the same file, it is not intended to evaluate your ability to write a data structure.
  */
 public class MyList {
-/**
+
+	String[] array;
+
+	public MyList() {
+		this.array = new String[0];
+	}
+	
+	/**
 	 * Adds the element as the first element of the list.
 	 * You don't need to implement this fully, just have something that is a start
 	 * using the appropriate structure.
 	 */
 	public void addFirst(String element) {
-		// Partner 1 should write this using a link structure.
-		// Partner 2 should write this using an array structure.
+		// Very bad implementation, make a new array with one more element than
+		// the current array, add the element at index 0 and then copy over the
+		// previous elements. The adjust this.array to point at the new array.
+		String[] newArray = new String[this.array.length + 1];
+		newArray[0] = element;
+		for (int i = 0; i < this.array.length; i++) {
+			newArray[i + 1] = this.array[i];
+		}
+		this.array = newArray;
 	}
 
 	/**
 	 * Removes the first element of the list.
-	 * You don't need to implement this fully, just have something that is a start
+	 * You don't need to implement this fully, must have something that is a start
 	 * using the appropriate structure.
 	 */
 	public void removeFirst() {
-		// Partner 1 should write this using a link structure.
-		// Partner 2 should write this using an array structure.
+		// Very bad implementation, make a new array with one less element than
+		// copy over the elements from the current array starting at index 1.
+		// Then adjust this.array to point at the new array.
+		if (this.array.length > 0) {
+			String[] newArray = new String[this.array.length - 1];
+			for (int i = 1; i < this.array.length; i++) {
+				newArray[i - 1] = this.array[i];
+			}
+			this.array = newArray;
+		}
+
 	}
 }
