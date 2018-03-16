53eec58cd636655a2b5511530738b32a1c2ed72e
Sureshdevendran <suresh@dhcp-10-132-153-144.uawifi.arizona.edu>
2018-03-16 09:38:23 -0700

    Partner 1 committing implementation of MyList

--- demoMediaPlayer/MyList.java
+++ demoMediaPlayer/MyList.java
@@ -8,23 +8,41 @@ package demoMediaPlayer;
  * to the same file, it is not intended to evaluate your ability to write a data structure.
  */
 public class MyList {
-/**
+
+	private class Node {
+		private String data;
+		private Node next;
+	}
+
+	private Node head;
+
+	public MyList() {
+		this.head = null;
+	}
+
+	/**
 	 * Adds the element as the first element of the list.
 	 * You don't need to implement this fully, just have something that is a start
 	 * using the appropriate structure.
 	 */
 	public void addFirst(String element) {
-		// Partner 1 should write this using a link structure.
-		// Partner 2 should write this using an array structure.
+		// Make a new node, with the element, then adjust head.
+		Node newHead = new Node();
+		newHead.data = element;
+		newHead.next = this.head;
+		this.head = newHead;
 	}
 
 	/**
 	 * Removes the first element of the list.
-	 * You don't need to implement this fully, just have something that is a start
+	 * You don't need to implement this fully, must have something that is a start
 	 * using the appropriate structure.
 	 */
 	public void removeFirst() {
-		// Partner 1 should write this using a link structure.
-		// Partner 2 should write this using an array structure.
+		// If the head isn't null, then adjust head to point at its next.
+		if (this.head != null) {
+			this.head = this.head.next;
+		}
 	}
+
 }
